cf1b58df8ab76093e27fc3b6c05caccb29b8ea7e
DJ Daemonix <dj.daemonix@nodsoft.net>
2020-05-22 21:35:42 +0200

    Fix sync_repo.sh script

--- sync_repo.sh
+++ sync_repo.sh
@@ -9,4 +9,4 @@ config_path=$client_path"config/"
 
 # Copy files to repo
 # cp -r $mods_path "$repo_path"/modpack/mods
-cp -r $config_path "$repo_path"/modpack/config/*
+cp -r "$config_path"* "$repo_path"/modpack/config/
